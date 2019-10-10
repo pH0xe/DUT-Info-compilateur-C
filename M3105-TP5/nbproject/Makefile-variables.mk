fb59d48df5790b2993411eef11dbea6721a7a7a7
REIG Julien <reigj@hostname.iut2.upmf-grenoble.fr>
2019-10-10 09:20:34 +0200

    ouais

--- M3105-TP5/nbproject/Makefile-variables.mk
+++ M3105-TP5/nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=None-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/None-Windows
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
 CND_ARTIFACT_NAME_Debug=m3105-tp5
-CND_ARTIFACT_PATH_Debug=dist/Debug/None-Windows/m3105-tp5
-CND_PACKAGE_DIR_Debug=dist/Debug/None-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/m3105-tp5
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
 CND_PACKAGE_NAME_Debug=m3105-tp5.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/None-Windows/package/m3105-tp5.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/m3105-tp5.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux
 CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
