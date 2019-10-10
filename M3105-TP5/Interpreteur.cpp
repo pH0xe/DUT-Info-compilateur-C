a17d94baf5a9e7dcce3cb26a3178057e6e8ef5d3
REIG Julien <reigj@hostname.iut2.upmf-grenoble.fr>
2019-10-10 09:51:02 +0200

    10_10_19

--- M3105-TP5/Interpreteur.cpp
+++ M3105-TP5/Interpreteur.cpp
@@ -56,7 +56,7 @@ Noeud* Interpreteur::seqInst() {
   NoeudSeqInst* sequence = new NoeudSeqInst();
   do {
     sequence->ajoute(inst());
-  } while (m_lecteur.getSymbole() == "<VARIABLE>" || m_lecteur.getSymbole() == "si");
+  } while (m_lecteur.getSymbole() == "<VARIABLE>" || m_lecteur.getSymbole() == "si" || m_lecteur.getSymbole() == "tantque");
   // Tant que le symbole courant est un début possible d'instruction...
   // Il faut compléter cette condition chaque fois qu'on rajoute une nouvelle instruction
   return sequence;
@@ -71,6 +71,8 @@ Noeud* Interpreteur::inst() {
   }
   else if (m_lecteur.getSymbole() == "si")
     return instSiRiche();
+  else if (m_lecteur.getSymbole() == "tantque")
+      return instTantQue();
 //  return instSi();
   // Compléter les alternatives chaque fois qu'on rajoute une nouvelle instruction
   else {
@@ -164,6 +166,17 @@ Noeud* Interpreteur::instSiRiche(){
   testerEtAvancer("finsi");
   
   return nullptr;
-    
+      
 }
 
+Noeud* Interpreteur::instTantQue(){
+    //      <instTantQue> ::=tantque( <expression> ) <seqInst> fintantque
+    testerEtAvancer("tantque");
+    testerEtAvancer("(");
+    Noeud* condition =  expression();
+    testerEtAvancer(")");
+    Noeud* sequence = seqInst();
+    testerEtAvancer("fintantque");
+    
+    return nullptr;
+}
